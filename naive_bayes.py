20ad2df114ea0d4296f8a7c76edcb681a4e66208
jlybianto <jesse.lybianto@gmail.com>
2016-03-06 15:39:06 -0700

    Add headings and import package for analysis

---
+++ naive_bayes.py
@@ -0,0 +1,16 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+# The pandas package is used to fetch and store data in a DataFrame.
+import pandas as pd
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+# Data Source: https://www.dropbox.com/s/hg0tqy6saqeoq0j/ideal_weight.csv?dl=0
+
+# ----------------
+# PROFILE DATA
+# ----------------
